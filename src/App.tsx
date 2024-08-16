1453ecb187b26f5d7a91b4d0c2b3397f1bae47d9
belal_soliman <belalsoliman8@gmail.com>
2024-08-16 12:02:42 +0300

    Add Range Iterator and Generator Components
    Introduce new components for generating and displaying ranges using both
    generator and iterator patterns. The `Iterator` component utilizes an iterator
    to generate a range of numbers, while the `Generator` component uses a generator
    function for the same purpose. Both components are integrated into the main
    application layout, enhancing the functionality and demonstrating different
    approaches to range generation in a React application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
+// components
 import Header from "./components/Header";
 
 import Toast from "./components/toast-task/ToastSection";
+import Iterator from "./components/range-task/Iterator";
 
 const App: React.FC = () => {
   return (
     <div className="container py-3">
       <Header />
 
-      <div className="flex flex-column sm:flex-row gap-3"></div>
+      <div className="flex flex-column sm:flex-row gap-3">
+        <Iterator />
+      </div>
 
       <Toast />
     </div>
